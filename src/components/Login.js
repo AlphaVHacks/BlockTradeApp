49d8c04c38f2f55528ee922bf03e04641b511492
Thomas H. Li <34053587+thomas-hai-li@users.noreply.github.com>
2020-05-30 17:56:51 -0400

    feat: implement user auth for login & signup (no redirect upon success)

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,25 +1,81 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+const sha256 = require('sha256');
 
-function Login() {
-  return (
-    <div className="Login">
-      <form>
-        <div className="form-group">
-          <label for="email">Email address</label>
-          <input type="email" className="form-control" id="email" />
-        </div>
-        <div className="form-group">
-          <label for="exampleInputPassword1">Password</label>
-          <input type="password" className="form-control" id="exampleInputPassword1" />
-        </div>
-        <button type="submit" className="btn btn-primary">Login</button>
-      </form>
-      <Link to="/signup">
-        <p>Not registered? Sign up here!</p>
-      </Link>
-    </div>
-  );
+class Login extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { email: '', password: '' };
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    const formFilled = this.state.email !== '' && this.state.password !== '';
+
+    if (formFilled) {
+      const email = this.state.email;
+      const hashedPassword = sha256(this.state.password);
+
+      fetch('http://localhost:5000/users')
+        .then(res => {
+          if (res.ok) return res.json();
+          else console.log(res);
+        })
+        .then(data => {
+          const user = data.find(user => user.email === email);
+
+          if (user) {
+            // check password
+            if (hashedPassword === user.password) {
+              console.log('authenticated!');
+              console.log(user);
+
+              // REDIRECT TO DASHBOARD
+
+            } else {
+              // prompt wrong password
+              alert('incorrect password')
+            }
+          } else {
+            // prompt no such user
+            alert('user doesnt exist');
+          }
+        })
+    } else {
+      // prompt to fill form
+      alert('please fill the form')
+    }
+  }
+
+  onEmailInput = (e) => {
+    this.setState({ email: e.target.value });
+  }
+
+  onPasswordInput = (e) => {
+    this.setState({ password: e.target.value });
+  }
+
+  render() {
+    return (
+      <div className="Login">
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label htmlFor="email">Email address</label>
+            <input type="email" className="form-control" id="email" onChange={this.onEmailInput} />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" className="form-control" id="password" onChange={this.onPasswordInput} />
+          </div>
+          <button className="btn btn-outline-primary">Login</button>
+        </form>
+        <Link to="/signup">
+          <p>Not registered? Sign up here!</p>
+        </Link>
+      </div>
+    );
+  }
 }
 
 export default Login;
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,29 +1,111 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+const sha256 = require('sha256');
 
-function Login() {
-  return (
-    <div className="Signup">
-      <form>
-        <div className="form-group">
-          <label for="name">Name</label>
-          <input type="name" className="form-control" id="name" />
-        </div>
-        <div className="form-group">
-          <label for="email">Email address</label>
-          <input type="email" className="form-control" id="email" />
-        </div>
-        <div className="form-group">
-          <label for="exampleInputPassword1">Password</label>
-          <input type="password" className="form-control" id="exampleInputPassword1" />
-        </div>
-        <button type="submit" className="btn btn-primary">Signup</button>
-      </form>
-      <Link to="/">
-        <p>Already have an account? Log in here!</p>
-      </Link>
-    </div>
-  );
+class Signup extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { name: '', email: '', password: '', publicKey: '', toDashboard: false };
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const formFilled = this.state.name !== '' && this.state.email !== '' && this.state.password !== '' && this.state.publicKey !== '';
+
+    if (formFilled) {
+      const name = this.state.name;
+      const email = this.state.email;
+      const password = sha256(this.state.password);
+      const publicKey = this.state.publicKey;
+
+      // check if email or publicKey is already in the DB
+      fetch('http://localhost:5000/users')
+        .then(res => {
+          if (res.ok) return res.json();
+          else console.log(res);
+        })
+        .then(data => {
+          const userSameEmail = data.find(user => user.email === email);
+          const userSamePublicKey = data.find(user => user.publicKey === publicKey);
+
+          if (userSameEmail || userSamePublicKey) {
+            alert('user with these credentials already exists')
+          } else {
+            // create a new account
+            const newUser = { name, email, password, publicKey }
+
+            fetch('http://localhost:5000/users/add', {
+              method: 'POST',
+              mode: 'cors',
+              cache: 'no-cache',
+              credentials: 'same-origin',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              redirect: 'follow',
+              referrerPolicy: 'no-referrer',
+              body: JSON.stringify(newUser)
+            })
+              .then(res => {
+                if (res.status === 200) {
+                  console.log('user created');
+                  console.log(newUser);
+
+                  // REDIRECT TO DASHBOARD
+
+                } else {
+                  alert('server error - failed to create user');
+                  console.log(res);
+                }
+              })
+          }
+        });
+    } else {
+      alert('please complete the form')
+    }
+
+    // this.setState({ toDashboard: true })
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  render() {
+    // if (this.state.toDashboard === true) {
+    //   return <Redirect to='/' />
+    // }
+
+    return (
+      <div className="Signup" >
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="name">Name</label>
+            <input type="name" className="form-control" name="name" onChange={this.handleChange} />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email address</label>
+            <input type="email" className="form-control" name="email" onChange={this.handleChange} />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" className="form-control" name="password" onChange={this.handleChange} />
+          </div>
+          <div className="form-group">
+            <label htmlFor="publicKey">Public Key</label>
+            <input type="publicKey" className="form-control" name="publicKey" onChange={this.handleChange} />
+          </div>
+          <button className="btn btn-outline-primary">Sign Up</button>
+        </form>
+        <Link to="/">
+          <p>Already have an account? Log in here!</p>
+        </Link>
+      </div>
+    );
+  }
 }
 
-export default Login;
+export default Signup;
