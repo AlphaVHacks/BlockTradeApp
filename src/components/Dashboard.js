69125a807f49d8738cdf48b59c6f413913833333
Syndicate555 <saziz028@uottawa.ca>
2020-05-31 04:30:06 -0400

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import StockCard from './StockCard';
-const alpha = require('alphavantage')({ key: 'AJC1VXSOOGI1OS2K' });
+import React from "react";
+import { Link } from "react-router-dom";
+import StockCard from "./StockCard";
+const alpha = require("alphavantage")({ key: "AJC1VXSOOGI1OS2K" });
 
 class Dashboard extends React.Component {
-
   constructor(props) {
     super(props);
-    this.state = { symbol: '', currency: 'USD', stockData: null };
+    this.state = { symbol: "", currency: "USD", stockData: null };
   }
 
   onSubmit = (e) => {
@@ -17,65 +16,91 @@ class Dashboard extends React.Component {
     const currency = this.state.currency;
 
     if (symbol !== "") {
-
       // get stock data in USD
-      alpha.data.quote(symbol)
-        .then(quoteData => {
-
+      alpha.data
+        .quote(symbol)
+        .then((quoteData) => {
           // perform currency conversion if needed
-          if (currency !== 'USD') {
-            alpha.forex.rate(currency, 'USD').then(exchangeData => {
-              const exchangeRate = Number(exchangeData["Realtime Currency Exchange Rate"]["5. Exchange Rate"]);
+          if (currency !== "USD") {
+            alpha.forex.rate(currency, "USD").then((exchangeData) => {
+              const exchangeRate = Number(
+                exchangeData["Realtime Currency Exchange Rate"][
+                  "5. Exchange Rate"
+                ]
+              );
 
               const stockDataWithAltCurrency = {
                 altCurrency: { currency, exchangeRate },
-                quote: quoteData
-              }
+                quote: quoteData,
+              };
 
               this.setState({ stockData: stockDataWithAltCurrency });
             });
           } else {
             this.setState({ stockData: quoteData });
           }
-
         })
-        .catch(err => {
-          this.setState({ stockData: 'err' });
-          console.log('Error: ' + err)
+        .catch((err) => {
+          this.setState({ stockData: "err" });
+          console.log("Error: " + err);
         });
     }
-  }
+  };
 
   onSymbolChange = (e) => {
     this.setState({ symbol: e.target.value });
-  }
+  };
 
   onCurrencyChange = (e) => {
     this.setState({ currency: e.target.value });
-  }
+  };
 
   render() {
-
     let stockData = this.state.stockData;
     let stockview;
 
     // no stock data
-    if (stockData === 'err') {
-      stockview = <p>No such stock symbol.</p>
+    if (stockData === "err") {
+      stockview = <p>No such stock symbol.</p>;
     } else if (stockData !== null) {
-      stockview = <StockCard stockData={stockData} />
+      stockview = <StockCard stockData={stockData} />;
+      return (
+        <div>
+          {stockview}
+          <button className="btn btn-outline-primary">Buy</button>
+        </div>
+      );
     }
 
     return (
       <div className="Dashboard">
-        <form style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }} onSubmit={this.onSubmit}>
+        <form
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+          onSubmit={this.onSubmit}
+        >
           <div className="form-group">
             <label htmlFor="symbol">Symbol (ie. GOOGL)</label>
-            <input style={{ width: '250px' }} type="symbol" className="form-control" id="symbol" onChange={this.onSymbolChange} />
+            <input
+              style={{ width: "250px" }}
+              type="symbol"
+              className="form-control"
+              id="symbol"
+              onChange={this.onSymbolChange}
+            />
           </div>
           <div className="form-group">
             <label htmlFor="currency">Currency</label>
-            <select style={{ width: '250px' }} className="form-control" id="currency" onChange={this.onCurrencyChange} value={this.state.currency}>
+            <select
+              style={{ width: "250px" }}
+              className="form-control"
+              id="currency"
+              onChange={this.onCurrencyChange}
+              value={this.state.currency}
+            >
               <option value="USD">USD</option>
               <option value="BTC">Bitcoin</option>
               <option value="ETH">Ethereum</option>
